aa2b7550f7c6570a31fc7264f1f54989b51a48ef
Xara Shaw <xaragamens30@gmail.com>
2024-11-07 13:58:22 -0800

    Changes

--- Assets/Scripts/shrimpThrower.cs
+++ Assets/Scripts/shrimpThrower.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class shrimpThrower : MonoBehaviour
 { 
@@ -9,6 +10,9 @@ public class shrimpThrower : MonoBehaviour
     [SerializeField] Transform spawnPointMid;
     [SerializeField] Transform spawnPointLeft;
     [SerializeField] TextAsset jsonFile;
+    [SerializeField] TMP_Text ScoreText;
+    private float shrimpThrown = 0;
+    private float shrimpShot = 0;
     [HideInInspector] public beatProcessing.Beats beatsRoot;
 
     int bpm = 150;
@@ -30,37 +34,43 @@ public class shrimpThrower : MonoBehaviour
 
         for (int i = 0;i < beatCount;i++)
         {
+            shrimpThrown += 1;
             var newShrimp = Instantiate(shrimp);
             var dir = 1;
+            var downDir = 1;
             shrimpList.Add(newShrimp);
 
             if (side == "left")
             {
                 newShrimp.transform.position = spawnPointLeft.position;
                 dir = 1;
+                downDir = 1;
             }
             else if (side == "right") 
             {
                 newShrimp.transform.position = spawnPointRight.position;
                 dir = -1;
+                downDir = 1;
             }
             else if (side == "mid")
             {
                 newShrimp.transform.position = spawnPointMid.position;
                 dir = 0;
-                StartCoroutine(midDelete(newShrimp));
+                downDir = -1;
+                //StartCoroutine(midDelete(newShrimp));
             }
 
             newShrimp.transform.localEulerAngles = new Vector3(0, 90, 0);
             var rb = newShrimp.gameObject.GetComponent<Rigidbody>();
             rb.AddTorque(new Vector3(Random.Range(-60, 60), Random.Range(-60, 60), Random.Range(-60, 60)));
-            rb.AddForce(new Vector3(Random.Range(dir * 400, dir * 300) * speed, 1500 * speed));
+            rb.AddForce(new Vector3(Random.Range(500, 300) * dir, 1500 * speed * downDir));
             yield return new WaitForSeconds(beatTime/beatCount);
         }
     }
 
     public void krill(GameObject shrimp)
     {
+        shrimpShot += 1;
         Destroy(shrimp);
     }
 
@@ -81,4 +91,8 @@ public class shrimpThrower : MonoBehaviour
 
     }
 
+    void Update()
+    {
+        ScoreText.text = "Accuracy: " + Mathf.CeilToInt((shrimpShot/shrimpThrown)*100).ToString() + "%";
+    }
 }
